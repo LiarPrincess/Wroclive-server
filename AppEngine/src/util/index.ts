1ba9d09e50c66bc13b45f0030f4f0498f49a59af
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2021-05-02 16:46:46 +0200

    [AE] Removed unused time constants

--- AppEngine/src/util/index.ts
+++ AppEngine/src/util/index.ts
@@ -1,4 +1,3 @@
 export * from './logger';
 export * from './environment-check';
-export * from './time';
 export * from './sleep';
--- AppEngine/src/util/time.ts
+++
@@ -1,3 +0,0 @@
-export const second = 1000;
-export const minute = 60 * second;
-export const hour = 60 * minute;

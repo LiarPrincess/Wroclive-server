6caedd9d6515940ba2f80174f54970af7861da5b
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-02-02 18:27:28 +0100

    [AE] Added 'LineLocationsAggregator'

---
+++ AppEngine/src/controllers/vehicle-locations-new/helpers/LineLocationsAggregator.ts
@@ -0,0 +1,43 @@
+import {
+  VehicleLocation,
+  VehicleLocationFromApi,
+  LineData,
+  LineLocations
+} from '../models';
+import { Line } from '../../lines';
+
+interface LineNameToIndex {
+  [key: string]: number | undefined;
+}
+
+export class LineLocationsAggregator {
+
+  private readonly lineLocations: LineLocations[];
+  private readonly lineNameToIndex: LineNameToIndex;
+
+  constructor() {
+    this.lineLocations = [];
+    this.lineNameToIndex = {};
+  }
+
+  addVehicle(line: Line, vehicle: VehicleLocation) {
+    const lineNameLower = line.name.toLowerCase();
+    let lineIndex = this.lineNameToIndex[lineNameLower];
+
+    if (lineIndex === undefined) {
+      const lineData = new LineData(line.name, line.type, line.subtype);
+      const lineLocations = new LineLocations(lineData, []);
+
+      lineIndex = this.lineLocations.length;
+      this.lineLocations.push(lineLocations);
+      this.lineNameToIndex[lineNameLower] = lineIndex;
+    }
+
+    const lineLocations = this.lineLocations[lineIndex];
+    lineLocations.vehicles.push(vehicle);
+  }
+
+  getLineLocations(): LineLocations[] {
+    return this.lineLocations;
+  }
+}

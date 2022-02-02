b7f1542bbe6303d226b4ab36312b6e673ef1c2da
LiarPrincess <4982138+LiarPrincess@users.noreply.github.com>
2022-02-02 19:12:27 +0100

    [AE] Added 'AngleCalculator'

---
+++ AppEngine/src/controllers/vehicle-locations-new/helpers/AngleCalculator.ts
@@ -0,0 +1,66 @@
+import { VehicleLocationFromApi } from '../models';
+import { calculateDistanceInMeters, calculateHeading } from '../math';
+
+/* ============== */
+/* === Config === */
+/* ============== */
+
+/** Min distance that vehicle has to move to update its heading (in meters). */
+export const minMovementToUpdateHeading = 30;
+
+/* ============== */
+/* === Types === */
+/* ============== */
+
+class VehicleLocation {
+  constructor(
+    public readonly lat: number,
+    public readonly lng: number,
+    public readonly angle: number
+  ) { }
+}
+
+interface VehicleLocationByVehicleId {
+  [key: string]: VehicleLocation | undefined;
+}
+
+/* ============ */
+/* === Main === */
+/* ============ */
+
+export class AngleCalculator {
+
+  /**
+   * Last place at which we updated vehicle angle/heading.
+   */
+  private lastVehicleAngleUpdateLocationById: VehicleLocationByVehicleId = {};
+
+  constructor() {
+    this.lastVehicleAngleUpdateLocationById = {};
+  }
+
+  calculateAngle(vehicle: VehicleLocationFromApi): number {
+    const id = vehicle.id;
+    const lat = vehicle.lat;
+    const lng = vehicle.lng;
+
+    const lastUpdateLocation = this.lastVehicleAngleUpdateLocationById[vehicle.id];
+    if (!lastUpdateLocation) {
+      const angle = 0.0;
+      this.lastVehicleAngleUpdateLocationById[id] = new VehicleLocation(lat, lng, angle);
+      return angle;
+    }
+
+    const oldLat = lastUpdateLocation.lat;
+    const oldLng = lastUpdateLocation.lng;
+
+    const movement = calculateDistanceInMeters(oldLat, oldLng, lat, lng);
+    if (movement < minMovementToUpdateHeading) {
+      return lastUpdateLocation.angle;
+    }
+
+    const angle = calculateHeading(oldLat, oldLng, lat, lng);
+    this.lastVehicleAngleUpdateLocationById[id] = new VehicleLocation(lat, lng, angle);
+    return angle;
+  }
+}
---
+++ AppEngine/src/controllers/vehicle-locations-new/helpers/index.ts
@@ -0,0 +1,4 @@
+export { AngleCalculator } from './AngleCalculator';
+export { IntervalErrorReporter } from './ErrorReporter';
+export { LineDatabase } from './LineDatabase';
+export { LineLocationsAggregator } from './LineLocationsAggregator';
